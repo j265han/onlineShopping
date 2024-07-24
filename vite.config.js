21bde246c36093f6583ba918e63b46fc2672c55a
hyj <2776232322@qq.com>
2024-07-24 16:59:52 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '/vue/index.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [
     vue(),
   ],
+  base:'/onlineShopping',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
